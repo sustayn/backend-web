ad35984f372749534823deaa30bb586fa08934d7
Jake Dluhy <dluhy2@illinois.edu>
2016-06-17 21:52:35 -0500

    Remove action mailer logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,6 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  config.action_mailer.logger = :log
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "corvae-backend_#{Rails.env}"
